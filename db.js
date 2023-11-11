105eef6d2d53846a278f334fd176e350b7120599
zakrnem <zakrnem@zohomail.com>
2023-11-10 20:54:39 -0600

    db.js connecting sucessfully

--- db.js
+++ db.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.URL}/test?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+});
+
+/*
 const client = new MongoClient(uri);
 
 const connectDB = async () => {
@@ -14,3 +24,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+*/
