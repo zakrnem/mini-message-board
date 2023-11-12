4c3d22e9af9b767ec31d947a26fe3d4ed098e1c1
zakrnem <zakrnem@zohomail.com>
2023-11-12 08:53:00 -0600

    Enable users to post messages

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.URL}/database?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB Atlas');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB Atlas");
 });
 
 const messageSchema = new mongoose.Schema({
   text: String,
   user: String,
   added: Date,
-})
+});
 
 module.exports = mongoose.model("Message", messageSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Message = require('../db')
-let messages
+const Message = require("../db");
 
-async function run () {
+run();
+async function run() {
   try {
-    const msg1 = await Message.findOne({user: "Amando"})
-    const msg2 = await Message.findOne({user: "Charles"})
+    const msg1 = await Message.findOne({ user: "Amando" });
+    const msg2 = await Message.findOne({ user: "Charles" });
 
     if (msg1 === null && msg2 === null) {
       await Message.create({
         text: "Hi there!",
         user: "Amando",
         added: new Date(),
-      })    
+      });
       await Message.create({
         text: "Hello World!",
         user: "Charles",
         added: new Date(),
-      })
-      console.log('Users created')
+      });
+      console.log("Users created");
     } else {
-      console.log('Users already exist')
+      console.log("Users already exist");
     }
-    messages = await Message.find()
   } catch (error) {
-    console.log("Error: ", error.message)
+    console.log("Error: ", error.message);
   }
 }
 
-renderMessages()
-async function renderMessages() {
-  await run()
-  /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Messageboard', messages });
-});
+renderMessages();
+function renderMessages() {
+  router.get("/", async function (req, res, next) {
+    const messages = await Message.find();
+    res.render("index", { title: "Messageboard", messages });
+  });
 }
 
-
-
-router.post('/new', function(req, res) {
-  const name = req.body.name
-  const message = req.body.message
-  messages.push({ 
+async function postMessages(message, name) {
+  await Message.create({
     text: message,
     user: name,
-    added: new Date()
-   })
-  console.log(messages)
-  res.redirect('/')
-})
+    added: new Date(),
+  });
+}
+router.post("/new", function (req, res) {
+  const name = req.body.name;
+  const message = req.body.message;
+  postMessages(message, name);
+  res.redirect("/");
+});
 
 module.exports = router;
