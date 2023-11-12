d93d011b09096fc136519cd7caa8c0753bea59a2
zakrnem <zakrnem@zohomail.com>
2023-11-12 07:56:04 -0600

    Implement document creation functionality on the db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.URL}/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.URL}/database?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {});
 
@@ -11,17 +11,10 @@ db.once('open', () => {
   console.log('Connected to MongoDB Atlas');
 });
 
-/*
-const client = new MongoClient(uri);
+const messageSchema = new mongoose.Schema({
+  text: String,
+  user: String,
+  added: Date,
+})
 
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-};
-
-module.exports = connectDB;
-*/
+module.exports = mongoose.model("Message", messageSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
+const Message = require('../db')
+
+run()
+async function run () {
+  try {
+    const msg1 = await Message.findOne({user: "Amando"})
+    const msg2 = await Message.findOne({user: "Charles"})
+
+    if (msg1 === null && msg2 === null) {
+      await Message.create({
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+      })    
+      await Message.create({
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+      })
+      console.log('Users created')
+    } else {
+      console.log('Users already exist')
+    }
+  } catch (error) {
+    console.log("Error: ", error.message)
   }
-];
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
